01d1c640e4f54865cf43aeeb2b0fb9939ebcd92f
Zandzakje <DamianTomasila@hotmail.com>
2020-11-19 10:41:16 +0100

    Project setup changed

---
+++ GameReviewAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GameReviewAuthentication.Models;
+//using AutoMapper;
+
+namespace GameReviewAuthentication.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/login")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public void TestFunction(LoginViewModel loginViewModel)
+        {
+            Console.WriteLine(loginViewModel.Username + ", " + loginViewModel.Password + " functie wordt aangeroepen! :D");
+        }
+    }
+}
