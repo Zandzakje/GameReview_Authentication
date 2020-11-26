16696ce87009578e241a96b6ef9c0faacbcfeea6
Zandzakje <DamianTomasila@hotmail.com>
2020-11-26 10:38:48 +0100

    before installing 'Microsoft.EntityFrameworkCore.SqlServer'

--- GameReviewAuthentication/Startup.cs
+++ GameReviewAuthentication/Startup.cs
@@ -27,6 +27,8 @@ namespace GameReviewAuthentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AuthenticationDbContext>();
+
             services.AddControllers();
 
             // Change between mock and database here
--- GameReviewAuthentication_Data/Repositories/AuthenticationDbContext.cs
+++ GameReviewAuthentication_Data/Repositories/AuthenticationDbContext.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GameReviewAuthentication_Data.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameReviewAuthentication_Data.Repositories
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : DbContext
     {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> opt) : base(opt)
+        {
 
+        }
+
+        public DbSet<LoginDTO> Logins { get; set; }
     }
 }
